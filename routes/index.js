4af87a7d2b56291f5330fbc74d3307151c38cd84
Aaron Nech <aaronnech@gmail.com>
2014-11-13 07:51:30 -0800

    fixed

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res) {
   res.render('splash');
 });
 
+// TODO: fix later
+router.get('/app', function(req, res) {
+    res.render('app');
+});
+
 module.exports = router;
