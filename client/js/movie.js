d74340d00a459710cc6e1f58421b2190db75280e
Aaron Nech <aaronnech@gmail.com>
2014-11-14 02:27:06 -0800

    fixed

--- client/js/movie.js
+++ client/js/movie.js
@@ -4,7 +4,8 @@ var ko = require('knockout');
 function Movie(title, youtubeUrl, data) {
     var self = this;
     self.data = data;
-    self.youtubeUrl = ko.observable("http://www.youtube.com/v/" + youtubeUrl.split('v=')[1]);
+    self.youtubeUrl =
+        ko.observable("http://www.youtube.com/v/" + youtubeUrl.split('v=')[1] + "?autoplay=1&controls=0&showinfo=0");
     self.title = ko.observable(title);
     self.friends = ko.observableArray();
 
