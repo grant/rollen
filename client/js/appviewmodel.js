f48e654aa48259d8a7ee232cef529c21dd50ee2e
Aaron Nech <aaronnech@gmail.com>
2014-11-13 22:50:11 -0800

    fixed

--- client/js/appviewmodel.js
+++ client/js/appviewmodel.js
@@ -1,130 +1,122 @@
-// var Server = require('./server');
-// var Friend = require('./friend');
-// var Movie = require('./movie');
-// var FlashMessage = require('./flashmessage');
-// var ko = require('knockout');
-// var $ = require('jquery');
+var Server = require('./server');
+var Friend = require('./friend');
+var Movie = require('./movie');
+var FlashMessage = require('./flashmessage');
+var ko = require('knockout');
+var $ = require('jquery');
 
 
-// function AppViewModel() {
-//   // PRIVATE
-//   //-------------
-//   var API_URL = 'localhost';
-//   var LEFT = 37;
-//   var RIGHT = 39;
+function AppViewModel() {
+   // PRIVATE
+   //-------------
+   var API_URL = 'localhost';
+   var LEFT = 37;
+   var RIGHT = 39;
 
 
-//   var ACCESS_TOKEN = $('#access-token').text();
-//   var self = this;
-//   var server = new Server(API_URL);
-//   var flash = new FlashMessage('flash', 2000);
+   var ACCESS_TOKEN = $('#access-token').text();
+   var self = this;
+   var server = new Server(API_URL);
+   var flash = new FlashMessage('flash', 2000);
 
-//   var disableSwipe = false;
+   var disableSwipe = false;
 
-//   var movies = [];
+   var movies = [];
 
-//   var getCurrentCard = function() {
-//     if (self.showFirstCard()) {
-//       return $('.card-one');
-//     } else {
-//       return $('.card-two');
-//     }
-//   };
+   var onShowDetail = function() {
+     self.showDetails(true);
+   };
 
-//   var onShowDetail = function() {
-//     self.showDetails(true);
-//   };
+   var maybeGetMoreMovies = function() {
+       server.getNextTrailers(function(movie) {
+           // Dequeue current movie
 
-//   var nextCard = function() {
-//     disableSwipe = true;
-//     $('.wrapper').animate({'top' : '0'}, 1000, function() {
-//       console.log('Moved card off screen: done');
+           // Update current movie and shift in the new movie
+           self.onNewMovie(movie);
+       });
+   };
 
-//       self.showFirstCard(!self.showFirstCard());
+   var nextCard = function() {
+     disableSwipe = true;
+     $('.film-roll').animate({'top' : '0'}, 1000, function() {
+       console.log('New card down: done');
 
-//       server.getNextTrailer(function(movie) {
-//         // Dequeue current movie
-//         movies.shift();
+       movies.shift();
+       maybeGetMoreMovies();
 
-//         // Update current movie and shift in the new movie
-//         self.onNewMovie(movie);
-//       });
+       $('.film-roll').css({'top' : '-100%'});
+       $('.frame').last().prepend('.film-roll');
+       disableSwipe = false;
+     });
+   };
 
-//       // Move the new card down
-//       getCurrentCard().animate({'top' : '0'}, 1000, function() {
-//         console.log('Moved new card on screen: done');
-//         disableSwipe = false;
-//       });
-//     });
-//   };
+   var flipCard = function() {
+     self.showDetails(!self.showDetails());
+   };
 
-//   var flipCard = function() {
-//     self.showDetails(!self.showDetails());
-//   };
+   // PUBLIC
+   //-------------
+   self.currentMovie = ko.observable(null);
+   self.showDetails = ko.observable(false);
+   self.makeEventName = ko.observable("");
 
-//   // PUBLIC
-//   //-------------
-//   self.currentMovie = ko.observable(null);
-//   self.showFirstCard = ko.observable(true);
-//   self.showDetails = ko.observable(false);
+   self.onNewMovie = function(movie) {
+     // Push in the new movie and update the current pointer
+     movies.push(movie);
+     self.currentMovie(movies[movies.length - 1]);
+     console.log(movies);
+   };
 
-//   self.onNewMovie = function(movie) {
-//     // Push in the new movie and update the current pointer
-//     movies.push(movie);
-//     self.currentMovie(movies[movies.length - 1]);
-//     console.log(movies);
-//   };
+   self.onRight = function() {
+     if (!disableSwipe) {
+       // First "Yes" flip
+       if (!self.showDetails()) {
+         flipCard();
+       } else { // Next card
+         self.onLeft();
+       }
+     }
+   };
 
-//   self.onRight = function() {
-//     if (!disableSwipe) {
-//       // First "Yes" flip
-//       if (!self.showDetails()) {
-//         flipCard();
-//       } else { // Next card
-//         self.onLeft();
-//       }
-//     }
-//   };
+   self.onLeft = function() {
+     if (!disableSwipe) {
+       nextCard();
+     }
+   };
 
-//   self.onLeft = function() {
-//     if (!disableSwipe) {
-//       nextCard();
-//     }
-//   };
+   // INIT
+   //-------------
 
-//   // INIT
-//   //-------------
+   flash.setMessage("Loading...");
+   flash.flash();
 
-//   flash.setMessage("Loading...");
-//   flash.flash();
+   $(document).keydown(function (e) {
+     if(e.which == LEFT) {
+       self.onLeft();
+     } else if(e.which == RIGHT) {
+       self.onRight();
+     }
+     return true;
+   });
 
-//   $(document).keydown(function (e) {
-//     if(e.which == LEFT) {
-//       self.onLeft();
-//     } else if(e.which == RIGHT) {
-//       self.onRight();
-//     }
-//     return true;
-//   });
+   server.getNextTrailer(function(movie) {
+     // Stop loading screen
+     $('.wrapper').css({'top' : -$('.wrapper').height() / 2});
+     $('.wrapper').show();
 
-//   server.getNextTrailer(function(movie) {
-//     // Stop loading screen
-//     $('.wrapper').css({'top' : -$('.wrapper').height() / 2});
-//     $('.wrapper').show();
+     // Load the movie into the queue
+     self.onNewMovie(movie);
 
-//     // Load the movie into the queue
-//     self.onNewMovie(movie);
+     // Grab another movie (preload)
+     server.getNextTrailer(function(movie) {
+       // Load the movie into the queue
 
-//     // Grab another movie (preload)
-//     server.getNextTrailer(function(movie) {
-//       // Load the movie into the queue
+       self.onNewMovie(movie);
 
-//       self.onNewMovie(movie);
+       flash.setMessage("Welcome!");
+       flash.flash();
+     });
+   });
+}
 
-//       flash.setMessage("Welcome!");
-//       flash.flash();
-//     });
-//   });
-// }
-
-// module.exports = AppViewModel;
+module.exports = AppViewModel;
--- client/js/friend.js
+++ client/js/friend.js
@@ -1,7 +1,10 @@
 var ko = require('knockout');
 
-function Friend() {
+function Friend(name, profilePicture) {
     var self = this;
+
+    self.name = ko.observable(name);
+    self.profilePicture = ko.observable(profilePicture);
 }
 
 module.exports = Friend;
