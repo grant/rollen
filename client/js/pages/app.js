9282ac45eb02da48606587604979f8b135236816
Grant Timmerman <granttimmerman@gmail.com>
2014-11-13 23:17:23 -0800

    uncomment

--- client/js/pages/app.js
+++ client/js/pages/app.js
@@ -1,6 +1,6 @@
-// var AppViewModel = require('../appviewmodel');
-// var ko = require('knockout');
+var AppViewModel = require('../appviewmodel');
+var ko = require('knockout');
 
-// var vm = new AppViewModel();
+var vm = new AppViewModel();
 
-// ko.applyBindings(vm);
+ko.applyBindings(vm);
