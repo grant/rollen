93af1c1590ec726a1ff3a47491576a3b5a14a62c
Grant Timmerman <granttimmerman@gmail.com>
2014-11-14 02:14:51 -0800

    cleanup js

--- client/js/appviewmodel.js
+++ client/js/appviewmodel.js
@@ -6,134 +6,150 @@ var $ = require('jquery');
 
 
 function AppViewModel() {
-   // PRIVATE
-   //-------------
-   var API_URL = 'localhost';
-   var LEFT = 37;
-   var RIGHT = 39;
+  // PRIVATE
+  //-------------
+  var API_URL = 'localhost';
+  var LEFT = 37;
+  var RIGHT = 39;
 
 
-   var ACCESS_TOKEN = $('#access-token').text();
-   var self = this;
-   var server = new Server(API_URL);
+  var ACCESS_TOKEN = $('#access-token').text();
+  var self = this;
+  var server = new Server(API_URL);
 
-   var disableSwipe = false;
+  var disableSwipe = false;
 
-   var movies = [];
+  // Queue for movies
+  var movies = [];
 
-   var onShowDetail = function() {
-     self.showDetails(true);
-   };
+  var onShowDetail = function() {
+    self.showDetails(true);
+  };
 
-   var maybeGetMoreMovies = function() {
-       if (movies.length < 6) {
-           server.getNextTrailers(function(movie) {
-               // Update current movie and shift in the new movie
-               self.onNewMovies(movie);
-           });
-       }
-   };
+  // Get more movies if below threshold
+  var maybeGetMoreMovies = function() {
+    if (movies.length < 6) {
+      server.getNextTrailers(function(movie) {
+        // Update current movie and shift in the new movie
+        self.onNewMovies(movie);
+      });
+    }
+  };
 
-   var nextMovie = function() {
-     self.firstMovie(!self.firstMovie());
-     movies.shift();
-     maybeGetMoreMovies();
-     if (self.firstMovie()) {
-       self.nextMovie(movies[1]);
-     } else {
-       self.currentMovie(movies[1]);
-     }
-     console.log(self.currentMovie());
-     console.log(self.nextMovie());
-     console.log(movies);
-   };
+  // 1. Remove current movie
+  // 2. Get more movies if necessary
+  // 3. Reset movie pointers
+  var nextMovie = function() {
+    self.firstMovie(!self.firstMovie());
+    movies.shift();
+    maybeGetMoreMovies();
+    if (self.firstMovie()) {
+      self.nextMovie(movies[1]);
+    } else {
+      self.currentMovie(movies[1]);
+    }
+    console.log(self.currentMovie());
+    console.log(self.nextMovie());
+    console.log(movies);
+  };
 
-   var nextCard = function() {
-     disableSwipe = true;
-     console.log('next card');
-     $('.film-roll').animate({'top' : '0'}, 1000, function() {
-       console.log('New card down: done');
+  // DOM stuff for moving to the next frame
+  // Called when preparing to move the film-reel to the next frame
+  var nextCard = function() {
+    disableSwipe = true;
+    console.log('next card');
+    $('.film-roll').animate({
+      'top': '0'
+    }, 1000, function() {
+      console.log('New card down: done');
 
-       nextMovie();
+      nextMovie();
 
-       $('.film-roll').css({'top' : '-100%'});
-       var $last = $('.frame').last();
-       var $first = $('.frame').first();
-       $first.before($last);
-       disableSwipe = false;
-     });
-   };
+      $('.film-roll').css({
+        'top': '-100%'
+      });
+      var $last = $('.frame').last();
+      var $first = $('.frame').first();
+      $first.before($last);
+      disableSwipe = false;
+    });
+  };
 
-   var flipCard = function() {
-     self.showDetails(!self.showDetails());
-   };
+  // Shows the movie details
+  var flipCard = function() {
+    self.showDetails(!self.showDetails());
+  };
 
-   // PUBLIC
-   //-------------
-   self.currentMovie = ko.observable(null);
-   self.nextMovie = ko.observable(null);
-   self.firstMovie = ko.observable(true);
-   self.showDetails = ko.observable(false);
-   self.makeEventName = ko.observable("");
+  // PUBLIC
+  //-------------
+  self.currentMovie = ko.observable(null);
+  self.nextMovie = ko.observable(null);
+  self.firstMovie = ko.observable(true);
+  self.showDetails = ko.observable(false);
+  self.makeEventName = ko.observable("");
 
-   self.onLikeMovie = function() {
-       flipCard();
-//       server.likeMovie(self.currentMovie(), function(success) {
-//         flipCard();
-//       });
-   };
+  // Event handler for liking a movie
+  self.onLikeMovie = function() {
+    flipCard();
+    //       server.likeMovie(self.currentMovie(), function(success) {
+    //         flipCard();
+    //       });
+  };
 
-   self.onNewMovies = function(movie) {
-     // Push in the new movie and update the current pointer
-     movies = movies.concat(movie);
+  // Event handler for getting new movies
+  self.onNewMovies = function(movie) {
+    // Push in the new movie and update the current pointer
+    movies = movies.concat(movie);
 
-     // Fill in friend fields for each movie
-     for(var i = 0; i < movie.length; i++) {
-         var currentMovie = movie[i];
-         (function(m) {
-           server.getFriendsWhoLiked(m, function(friends) {
-             m.setFriends(friends);
-           });
-         })(currentMovie);
-     }
-   };
+    // Fill in friend fields for each movie
+    for (var i = 0; i < movie.length; i++) {
+      var currentMovie = movie[i];
+      (function(m) {
+        server.getFriendsWhoLiked(m, function(friends) {
+          m.setFriends(friends);
+        });
+      })(currentMovie);
+    }
+  };
 
-   self.onRight = function() {
-     if (!disableSwipe) {
-       // First "Yes" flip
-       if (!self.showDetails()) {
-         self.onLikeMovie();
-       } else { // Next card
-         self.onLeft();
-       }
-     }
-   };
+  // Event handler for pressing right arrow key
+  self.onRight = function() {
+    if (!disableSwipe) {
+      // First "Yes" flip
+      if (!self.showDetails()) {
+        self.onLikeMovie();
+      } else { // Next card
+        self.onLeft();
+      }
+    }
+  };
 
-   self.onLeft = function() {
-     console.log('LEFT');
-     if (!disableSwipe) {
-       nextCard();
-     }
-   };
+  // Event handler for pressing right arrow key
+  self.onLeft = function() {
+    console.log('LEFT');
+    if (!disableSwipe) {
+      nextCard();
+    }
+  };
 
-   // INIT
-   //-------------
+  // INIT
+  //-------------
 
-   $(document).keydown(function (e) {
-     if(e.which == LEFT) {
-       self.onLeft();
-     } else if(e.which == RIGHT) {
-       self.onRight();
-     }
-     return true;
-   });
+  $(document).keydown(function(e) {
+    if (e.which == LEFT) {
+      self.onLeft();
+    } else if (e.which == RIGHT) {
+      self.onRight();
+    }
+    return true;
+  });
 
-   server.getNextTrailers(function(movies) {
-     // Load the movie into the queue
-     self.onNewMovies(movies);
-     self.currentMovie(movies[0]);
-     self.nextMovie(movies[1]);
-   });
+  server.getNextTrailers(function(data) {
+    // Load the movie into the queue
+    self.onNewMovies(data);
+    self.currentMovie(movies[0]);
+    self.nextMovie(movies[1]);
+  });
 }
 
 module.exports = AppViewModel;
