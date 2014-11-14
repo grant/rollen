982715918c8520afc7e566d345e049c4f7e54e4c
Karan Goel <karan@goel.im>
2014-11-13 20:32:25 -0800

    add endpoint for geting queue

--- routes/index.js
+++ routes/index.js
@@ -29,3 +29,22 @@ exports.authError = function(req, res) {
 exports.authSuccess = function(req, res) {
   res.redirect('/app');
 };
+
+exports.getMovies = function(req, res) {
+  // return top 3 from queue
+  // move these top 3 from queue to seen
+  User.findOne({id: req.user.id}, function(err, user) {
+    var outPut = user.queue.splice(0, 3);
+    var remainder = user.queue.splice(3);
+
+    user.queue = remainder;
+    user.seen.concat(outPut);
+
+    user.save(function(err, newUser) {
+      res.json({
+        'queue': outPut
+      });
+    });
+
+  });
+}
